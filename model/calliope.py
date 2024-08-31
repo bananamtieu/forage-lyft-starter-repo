100a44fdff910f3f022884d37245d098e5295deb
bananamtieu <emmett.tieu0905@gmail.com>
2024-08-30 17:12:17 -0700

    rewrite

--- model/calliope.py
+++ model/calliope.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-
+from serviceable import Serviceable
 from engine.capulet_engine import CapuletEngine
+from battery.spindler_battery import SpindlerBattery
 
+class Calliope(Serviceable):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
+        self.engine = CapuletEngine(last_service_mileage, current_mileage)
+        self.battery = SpindlerBattery(last_service_date, datetime.today().date())
 
-class Calliope(CapuletEngine):
-    def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+    def needs_service(self) -> bool:
+        return self.engine.needs_service() or self.battery.needs_service()
