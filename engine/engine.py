1c3fac19f1446cca7989fb9fc99b9267c30ea997
bananamtieu <emmett.tieu0905@gmail.com>
2024-08-30 17:07:57 -0700

    rename

--- engine/engine.py
+++ engine/engine.py
@@ -3,6 +3,6 @@ from abc import ABC, abstractmethod
 # Define the abstract Engine class
 class Engine(ABC):
     @abstractmethod
-    def needs_service(self) -> bool:
+    def engine_should_be_serviced(self) -> bool:
         """Determine if the engine needs servicing."""
         pass
